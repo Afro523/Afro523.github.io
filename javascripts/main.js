3780c036d3cef3d53d508ed6153dae003b0d0de8
Afro523 <Jresnick523@gmail.com>
2015-12-21 01:36:19 -0500

    A Slew of changes
    changes to js, css, and index page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+(function() {
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+    });
+  }
+
+})();
