0036cfb06eb1a660c0e842aa02d99f5822d7a915
Afro523 <Jresnick523@gmail.com>
2016-01-15 20:27:45 -0500

    so much
    stufffff

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
 var minifyCss = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
 
 gulp.task('default', function() {
 });
 
-  gulp.task('compress', function() {
+
+  gulp.task('compressJS', function() {
     return gulp.src('javascripts/*.js')
-      .pipe(minify({
-        ignoreFiles: ['-min.js']
-      }))
-      .pipe(gulp.dest('minified'))
-});
-  gulp.task('compress', function() {
+          .pipe(minify())
+          .pipe(gulp.dest('minified'))
+        });
+
+  gulp.task('compressCss', function() {
     return gulp.src('stylesheets/*.css')
         .pipe(minifyCss())
+        .pipe(rename(function (path){
+          path.basename += "-min";
+        }))
         .pipe(gulp.dest('minified'))
 
 });
