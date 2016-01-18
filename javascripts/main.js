3a219ed56267b85204f30466921d96daf796a2bf
Afro523 <Jresnick523@gmail.com>
2016-01-18 14:45:01 -0500

    staff
    woo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,28 @@
 
   function expandToggle(){
     var projectState = document.getElementById('projectCellsHolder');
+    var aboutMeState = document.getElementById('aboutParagraph');
 
     if (projectState.style.display == 'block') {
-      //Hidden
+      //Set To Hidden
       document.getElementById('projectCellsHolder').style.display = 'none';
     }else {
-      //Showing
+      //Set to Showing
       document.getElementById('projectCellsHolder').style.display = 'block';
+
+      if (aboutMeState.style.display != 'none'){
+        aboutMeState.style.display = 'none';
+      }
+    }
+
+  };
+
+  function toggleController(){
+    var toggleArray[] = document.getElementsByClassName('toggleable');
+    var arrayLength = toggleArray.length;
+
+    //Turn everything Off
+    for (x; x <= arrayLength; x++){
+      toggleArray[x].style.display = 'none';
     }
   };
