1bbd0193e0eea2afa23cfba0537be34ce8135ce9
Afro523 <Jresnick523@gmail.com>
2016-01-27 18:19:10 -0500

    with minified
    forgot to gulp

--- minified/main.js
+++ minified/main.js
@@ -15,7 +15,38 @@ function toggleController(headerClicked){
         }
     }
   } catch (e) {
-
     }
-
   };
+
+function scrollStart(element) {
+    var ele = typeof element === "string" ? document.getElementById(element) : element;
+    var DistanceNeededY = ele.offsetTop * 0.80;
+
+    //Experimental
+    var scrollStep = Math.PI / (1000/15),
+    cosParameter = DistanceNeededY /2;
+
+    console.log('scrollStep = ' + scrollStep);
+    console.log('cosParameter = ' + cosParameter);
+
+    var scrollCount = 0,
+        scrollMargin,
+        scrollInterval = setInterval( function() {
+          if (DistanceNeededY != 0) {
+            scrollCount = scrollCount + 1;
+            scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+            window.scrollTo( 0, ( DistanceNeededY - scrollMargin ) );
+            console.log('DistanceNeededY = ' + DistanceNeededY);
+            console.log('scrollCount = ' + scrollCount);
+            console.log('scrollMargin = ' + scrollMargin);
+            console.log('scrollInterval = ' + scrollInterval);
+            console.log('scrollCount = ' + scrollCount);
+          } else {
+            clearInterval(scrollInterval);
+          }
+        }, 15);
+
+  //  window.scrollTo(0, ele.offsetTop * 0.75);
+
+
+}
